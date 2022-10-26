cd98305bf3e3689e7ef4735e1cfcfd781dc16f34
monginadiana <dianahmongina2@gmail.com>
2022-10-26 14:46:23 +0300

    controller validations

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,6 @@
 class Post < ApplicationRecord
+
+    validates :title , presence: true
+    validates :content, length: {minimum:100}
+    validates :category, inclusion: { in: %w(Fiction Non-Fiction ), message: "is not included in the list" }
 end
